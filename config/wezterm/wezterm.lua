5f24cf15cfaea1d1b8bc5c2984ce42f276b2eb25
Chad Jones <chad.jones@tempursealy.com>
2025-04-16 10:03:35 +0200

    cross platform wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,33 +9,7 @@ config.color_scheme = 'carbonfox'
 -- window
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
--- key mapping
-config.keys = {
-  { key = "d",          mods = "SHIFT|CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-  { key = "d",          mods = "CMD",       action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-  { key = "DownArrow",  mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Down") },
-  { key = "UpArrow",    mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Up") },
-  { key = "LeftArrow",  mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Left") },
-  { key = "RightArrow", mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Right") },
-  { key = "w",          mods = "CMD",       action = act.CloseCurrentPane({ confirm = true }) },
-  { key = "r",          mods = "LEADER",    action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
-  { key = "Enter",      mods = "LEADER",    action = act.ActivateCopyMode },
-  { key = "c",          mods = "CMD",       action = act.CopyTo("Clipboard") },
-  { key = "n",          mods = "CMD",       action = act.SpawnWindow },
-  { key = "v",          mods = "CMD",       action = act.PasteFrom("Clipboard") },
-  {
-    key = ",",
-    mods = "LEADER",
-    action = act.PromptInputLine({
-      description = "Enter new name for tab",
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    }),
-  },
-}
+
 config.mouse_bindings = {
   -- Ctrl-click will open the link under the mouse cursor
   {
@@ -47,7 +21,38 @@ config.mouse_bindings = {
 
 -- Windows
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-end
+  -- IMPORTANT: Sets WSL2 UBUNTU as the defualt when opening Wezterm
+  config.default_domain = 'WSL:Ubuntu'
+  config.color_scheme_dirs = { '/mnt/c/Users/chad.jones/colors' }
+  -- key mapping
+  config.keys = {
+    { key = "d",          mods = "SHIFT|ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "d",          mods = "ALT",       action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "DownArrow",  mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "UpArrow",    mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "LeftArrow",  mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "RightArrow", mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "w",          mods = "ALT",       action = act.CloseCurrentPane({ confirm = true }) },
+    { key = "r",          mods = "LEADER",    action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+    { key = "Enter",      mods = "LEADER",    action = act.ActivateCopyMode },
+    { key = "c",          mods = "ALT",       action = act.CopyTo("Clipboard") },
+    { key = "n",          mods = "ALT",       action = act.SpawnWindow },
+    { key = "t",          mods = "ALT",       action = act.SpawnTab 'DefaultDomain' },
+    { key = "v",          mods = "ALT",       action = act.PasteFrom("Clipboard") },
+    {
+      key = ",",
+      mods = "LEADER",
+      action = act.PromptInputLine({
+        description = "Enter new name for tab",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      }),
+    },
+  }
+  end
 
 -- Apple Silicon Mac
 if wezterm.target_triple == 'aarch64-apple-darwin' then
@@ -57,11 +62,41 @@ if wezterm.target_triple == 'aarch64-apple-darwin' then
     weight = 'Regular',
   }
   config.font_size = 14.0
-  config.macos_window_background_blur = 20
-end
+  config.macos_window_background_blur = 20-- key mapping
+  config.keys = {
+    { key = "d",          mods = "SHIFT|CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "d",          mods = "CMD",       action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "DownArrow",  mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "UpArrow",    mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "LeftArrow",  mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "RightArrow", mods = "SHIFT",     action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "w",          mods = "CMD",       action = act.CloseCurrentPane({ confirm = true }) },
+    { key = "r",          mods = "LEADER",    action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+    { key = "Enter",      mods = "LEADER",    action = act.ActivateCopyMode },
+    { key = "c",          mods = "CMD",       action = act.CopyTo("Clipboard") },
+    { key = "n",          mods = "CMD",       action = act.SpawnWindow },
+    { key = "t",          mods = "CMD",       action = act.SpawnTab 'DefaultDomain' },
+    { key = "v",          mods = "CMD",       action = act.PasteFrom("Clipboard") },
+    {
+      key = ",",
+      mods = "LEADER",
+      action = act.PromptInputLine({
+        description = "Enter new name for tab",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      }),
+    },
+  }
+  end
 
 -- Linux
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
 end
+
+-- stop the noise
+config.audible_bell = "Disabled"
 
 return config
