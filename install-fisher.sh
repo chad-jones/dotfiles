26c16751736668d640c06d0ee5b7f6d709f58bcd
Chad Jones <chad@crashcode.org>
2025-05-10 07:43:29 +0200

    remove sponge

--- install-fisher.sh
+++ install-fisher.sh
@@ -4,6 +4,5 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install edc/bass
 fisher install jorgebucaran/nvm.fish
-fisher install meaningful-ooo/sponge
 fisher install evanlucas/fish-kubectl-completions
 fisher install nickeb96/puffer-fish
